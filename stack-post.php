92c756544386dd6e1ead8c77f22c502cc82de407
Jordreed002 <jordan@lineindustries.com>
2022-05-17 18:05:04 +0100

    Update stack-post.php

--- stack-post.php
+++ stack-post.php
@@ -19,7 +19,7 @@ get_header(); ?>
   
         <div class="another-read-stack-post">
             <div class="stack-title">
-                <h1><?php echo $title; ?></h1>
+                <h2 class="post-title"><?php echo $title; ?></h2>
             </div>
             <div class="stack-content">
             <?php foreach($stackContent['book_list'] as $stack){ ?>
@@ -28,25 +28,25 @@ get_header(); ?>
                     <div class="book-image">
                         <a href="<?php echo $stack['book_link'] ?>"><img src="<?php echo $stack['jacket_image']; ?>" alt="<?php echo $stack['book_name']; ?>"></a>
                     </div>
-                    <div class="book-text">
-                        <p><?php if(isset($options['keynote']) && $options['keynote'] == '1'){echo $stack['keynote'];} ?></p>
-                    </div>
                     <div class="book-info">
-                        <div class="activity-book-title">
+                        <div class="book-title">
                             <a href="<?php echo $stack['book_link']; ?>">
                                 <p><?php echo $stack['book_name']; ?></p>
                             </a>
                         </div>
 
                         <?php foreach($stack['contributors'] as $contributor){ ?>
-                        <div class="stbook-author">
+                        <div class="book-author">
                             <a href="<?php echo $contributor['author_link']; ?>">
-                                <p><?php echo $contributor['author_name']; ?></p>
+                                <h3 class="author-name"><?php echo $contributor['author_name']; ?></h3>
                             </a>
                         </div>
 
                         <?php } ?>
 
+                    </div>
+                    <div class="book-text">
+                        <p><?php if(isset($options['keynote']) && $options['keynote'] == '1'){echo $stack['keynote'];} ?></p>
                     </div>
                 </div>
                 <?php } ?>
